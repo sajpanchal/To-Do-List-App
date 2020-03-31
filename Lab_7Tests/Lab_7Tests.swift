a0b562a6ca3cff594dc0b3075651f802c05a26db
Saj Panchal <sajpanchal@gmail.com>
2020-03-31 19:16:55 -0400

    Initial Commit

---
+++ Lab_7Tests/Lab_7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Lab_7Tests.swift
+//  Lab_7Tests
+//
+//  Created by saj panchal on 2020-03-31.
+//  Copyright © 2020 saj panchal. All rights reserved.
+//
+
+import XCTest
+@testable import Lab_7
+
+class Lab_7Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
